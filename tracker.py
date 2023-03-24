58f1ba8b6935a64bacf1ab4d4450e36a9abe544d
monicaiizuka <123885703+monicaiizuka@users.noreply.github.com>
2023-03-23 21:53:54 -0400

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -8,7 +8,7 @@ def print_usage():
     ''' print an explanation of how to use this command '''
     print('''usage:
             tracker quit
-            tracker showall categories
+            tracker showall categories  
             tracker add_category
             tracker modify category
             tracker show transactions
@@ -39,23 +39,65 @@ def process_args(arglist):
     transaction = Transaction()
     if arglist==[]:
         print_usage()
-    elif arglist[0]=="showall categories":
+        
+    elif arglist[0]=="showall":  #shows all the categories
         print_trackers(trackers = transaction.distinctCategories())
-    elif arglist[0]=="add_category":
-        if len(arglist)!=1:
-            print_usage()
-        else:
-            tracker =
-    elif arglist[0]=="add_transaction":
-        if len(arglist)!=1:
-            print_usage()
-        else:
-    elif arglist[0]=="delete":
+        
+    elif arglist[0]=="add_category":   #adds the category 
         if len(arglist)!=2:
             print_usage()
         else:
+            item = int(arglist[1])
+            cat = arglist[2]
+            transaction.addCategory(item, cat)
+    
+    elif arglist[0]=="modify":  #modifys the category
+        if len(arglist)!=3:
+            print_usage()
+        else:
+            transaction.addCategory(arglist[1], arglist[2])
             
+    elif arglist[0]=="show": #shows the transactions 
+        print_trackers(trackers=transaction.selectAll())
 
+    elif arglist[0]=="add_transaction":  #adds the transactions 
+        if len(arglist)!= 6:
+            print_usage()
+        else: 
+            item = int(arglist[1])
+            amount = float(arglist[2])
+            cat = arglist[3]
+            date = arglist[4]
+            desc = arglist[5]
+            transaction.addTransaction({'item': item, 'amount': amount, 'category': cat, 'date': date, 'description': desc})
+            
+    elif arglist[0]=="delete": #deletes the transaction
+        if len(arglist)!=2:
+            print_usage()
+        else:
+            transaction.deleteTransaction(int(arglist[1]))
+          
+    elif arglist[0]=="summarize": 
+        if len(arglist)!=2:
+             print_usage()
+        elif arglist[1]=="by_date":   #summarize by date 
+            print_trackers(trackers = transaction.byDate())
+        elif arglist[1]=="by_month":  #summarize by month
+            print_trackers(trackers = transaction.byMonth())
+        elif arglist[1]=="by_year":   #summarize by year
+            print_trackers(trackers = transaction.byYear())
+        elif arglist[1]=="by_category": #summarize by category
+            print_trackers(trackers = transaction.distinctCategories())
+        else:
+            print_usage()
+            
+    elif arglist[0]=="quit":  #exits the program 
+        sys.exit()
+        
+    else:
+        print_usage()
+        
+        
 def toplevel():
     ''' read the command args and process them'''
     if len(sys.argv)==1:
