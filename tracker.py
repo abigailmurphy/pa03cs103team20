74aa3eb2fe8d221503f626177d487108fd586a41
monicaiizuka <123885703+monicaiizuka@users.noreply.github.com>
2023-03-23 21:06:10 -0400

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -51,5 +51,25 @@ def process_args(arglist):
         else:
             
 
+def toplevel():
+    ''' read the command args and process them'''
+    if len(sys.argv)==1:
+        # they didn't pass any arguments, 
+        # so prompt for them in a loop
+        print_usage()
+        args = []
+        while args!=['']:
+            args = input("command> ").split(' ')
+            if args[0]=='add':
+                # join everyting after the name as a string
+                args = ['add',args[1]," ".join(args[2:])]
+            process_args(args)
+            print('-'*40+'\n'*3)
+    else:
+        # read the args and process them
+        args = sys.argv[1:]
+        process_args(args)
+        print('-'*40+'\n'*3)
 
+toplevel()
 
