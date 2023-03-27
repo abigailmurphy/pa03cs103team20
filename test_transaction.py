cf32fbabb6eb8039403fca6a664c5911afd46e51
monicaiizuka <123885703+monicaiizuka@users.noreply.github.com>
2023-03-26 21:11:19 -0400

    Update test_transaction.py

--- test_transaction.py
+++ test_transaction.py
@@ -2,9 +2,10 @@ import pytest
 from transaction import Transaction
 
 def test_to_dict():
-  
-  
-  
+  # Test that the to_dict function returns a dictionary with the correct keys and values
+    t = (1, 10.0, 'food', '2022-03-26', 'groceries')
+    expected_result = {'item_num': 1, 'amount': 10.0, 'category': 'food', 'date': '2022-03-26', 'description': 'groceries'}
+    assert Transaction(database).to_dict(t) == expected_result
   
 def test_select_all():
   
