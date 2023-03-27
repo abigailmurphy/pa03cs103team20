ae235c8dc4545e6c674d225893c4e36464aac293
abigailmurphy <abigailmurphy@brandeis.edu>
2023-03-26 20:34:04 -0400

    Update transaction.py

--- transaction.py
+++ transaction.py
@@ -3,7 +3,7 @@ import sqlite3
 import os
 
 def to_dict(t):
-    ''' t is a tuple (item_num, amount, category,date,description)'''
+    ''' t is a tuple (item_num, amount, category,date,description) ~abigailmurphy'''
     print('t='+str(t))
     tracker = {'item_num':t[0], 'amount':t[1], 'category':t[2], 'date':t[3], 'description':t[4]}
     return tracker
@@ -11,51 +11,52 @@ def to_dict(t):
 class Transaction:
 
     def __init__(self, database):
+        '''~abigailmurphy'''
         self.dbase = database
         self.run_query('''CREATE TABLE IF NOT EXISTS transactions
                     (item_num INT PRIMARY KEY, amount DOUBLE, category TEXT, date DATE, description TEXT)''',() )
         
 
     def select_all(self):
-        ''' return all items in tracker as a list of dicts'''
+        ''' return all items in tracker as a list of dicts ~abigailmurphy'''
         return self.run_query("SELECT * FROM transactions",())
 
     def select_item(self):
-        ''' return all item #'s as a list of dicts.'''
+        ''' return all item #'s as a list of dicts. ~abigailmurphy'''
         return self.run_query("SELECT item from transactions",())
    
     def distinct_categories(self):
-        ''' return all distinct categories as a list of dicts.'''
+        ''' return all distinct categories as a list of dicts. ~abigailmurphy'''
         return self.run_query("SELECT DISTINCT category from transactions",())
       
     def by_date(self):
-        ''' return all distinct categories as a list of dicts.'''
+        ''' return all distinct categories as a list of dicts. ~abigailmurphy'''
         return self.run_query("SELECT item_num, amount, category, date, description FROM transactions ORDER BY date",())
       
     def by_month(self):
-        ''' return all distinct categories as a list of dicts.'''
+        ''' return all distinct categories as a list of dicts. ~abigailmurphy'''
         return self.run_query("SELECT DISTINCT item_num, amount, category, date, description FROM transactions ORDER BY MONTH(date) DESC",())
       
     def by_month_asc_year(self):
-        ''' return all distinct categories as a list of dicts.'''
+        ''' return all distinct categories as a list of dicts. ~abigailmurphy'''
         return self.run_query("SELECT DISTINCT item_num, amount, category, date, description FROM transactions ORDER BY YEAR(date) DESC",())
       
     def add_category(self, item_num, cat):
-        ''' return updated with category added '''
+        ''' return updated with category added ~abigailmurphy, ariasmithbrandeis'''
         return self.run_query("UPDATE transactons SET category = (?) WHERE item_num=(?)",(cat, item_num,))
 
    
     def add_transaction(self,item):
-        ''' create a transactions item and add it to the transactions table '''
+        ''' create a transactions item and add it to the transactions table ~abigailmurphy'''
         return self.run_query("INSERT INTO transactions VALUES(?,?,?,?,?)",(item['item_num'],item['amount'],item['category'],item['date'],item['description']))
 
     def delete_transaction(self,item_num):
-        ''' delete a transactions item '''
+        ''' delete a transactions item '~abigailmurphy'''
         return self.run_query("DELETE FROM transactions WHERE item_num=(?)",(item_num,))
 
 
     def run_query(self,query,tuple):
-        ''' return all of the uncompleted tasks as a list of dicts.'''
+        ''' return all of the uncompleted tasks as a list of dicts. ~abigailmurphy, ariasmithbrandeis'''
         con= sqlite3.connect(os.getenv('HOME')+'/'+ self.dbase)
         cur = con.cursor() 
         cur.execute(query,tuple)
