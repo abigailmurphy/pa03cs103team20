0869e084b381bd47313656292e1568234cafd6d7
monicaiizuka <123885703+monicaiizuka@users.noreply.github.com>
2023-03-26 21:21:58 -0400

    Update test_transaction.py

--- test_transaction.py
+++ test_transaction.py
@@ -1,6 +1,9 @@
 import pytest
 from transaction import Transaction
 
+def database():
+    return Transaction('example')
+  
 def test_to_dict():
   # Test that the to_dict function returns a dictionary with the correct keys and values
     t = (1, 10.0, 'food', '2022-03-26', 'groceries')
@@ -8,14 +11,28 @@ def test_to_dict():
     assert Transaction(database).to_dict(t) == expected_result
   
 def test_select_all():
-  
-
-
+  # Add some test transactions to the database
+    database.add_transaction({'item_num': 1, 'amount': 10.0, 'category': 'Groceries', 'date': '2022-03-25', 'description': 'Bought groceries'})
+    database.add_transaction({'item_num': 2, 'amount': 20.0, 'category': 'Transportation', 'date': '2022-03-26', 'description': 'Filled up gas'})
+    database.add_transaction({'item_num': 3, 'amount': 30.0, 'category': 'Entertainment', 'date': '2022-03-27', 'description': 'Watched a movie'})
+    expected = [
+        {'item_num': 1, 'amount': 10.0, 'category': 'Groceries', 'date': '2022-03-25', 'description': 'Bought groceries'},
+        {'item_num': 2, 'amount': 20.0, 'category': 'Transportation', 'date': '2022-03-26', 'description': 'Filled up gas'},
+        {'item_num': 3, 'amount': 30.0, 'category': 'Entertainment', 'date': '2022-03-27', 'description': 'Watched a movie'}
+    ]
+    assert database.select_all() == expected
 
 def test_select_item():
-  # Test that the select_item function returns a list of all item numbers as dictionaries
-    expected_result = [{'item_num': 1}, {'item_num': 2}, {'item_num': 3}]
-    assert Transaction(database).select_item() == expected_result
+  # Add some test transactions to the database
+    database.add_transaction({'item_num': 1, 'amount': 10.0, 'category': 'Groceries', 'date': '2022-03-25', 'description': 'Bought groceries'})
+    database.add_transaction({'item_num': 2, 'amount': 20.0, 'category': 'Transportation', 'date': '2022-03-26', 'description': 'Filled up gas'})
+    database.add_transaction({'item_num': 3, 'amount': 30.0, 'category': 'Entertainment', 'date': '2022-03-27', 'description': 'Watched a movie'})
+    expected = [
+        {'item': 1},
+        {'item': 2},
+        {'item': 3}
+    ]
+    assert database.select_item() == expected
   
 
 def test_distinct_categories():
